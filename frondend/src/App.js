f8ddd6b3426527031c5c3178fa4a4311db428f45
MdNazmul9 <nazmul.cse48@gmail.com>
2022-04-05 19:59:41 +0600

    react boiler plate initialize

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -6,11 +6,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import Home from "./components/Home";
+import Login from "./components/login/Login"
+import Signup from './components/signup/Signup';
+import Dashboard from './components/dashboard/Dashboard';
+
 const App = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="home/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
       </Routes>
     </Suspense>
