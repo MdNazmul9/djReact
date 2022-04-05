92a24214fb621fe283fd43ad2c61fcf767738532
MdNazmul9 <nazmul.cse48@gmail.com>
2022-04-05 20:10:07 +0600

    signup page

--- frondend/src/components/Home.js
+++ frondend/src/components/Home.js
@@ -1,15 +1,24 @@
-import React, {Component} from 'react';
-import { Container } from 'react-bootstrap';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 class Home extends Component {
-   
-    render() { 
-        return (  
-            <Container>
-                <h1>Home</h1>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <h1>Home</h1>
+        <p>
+          <NavLink to="/login/">Login</NavLink>
+        </p>
+        <p>
+          <NavLink to="/signup">Sign up</NavLink>
+        </p>
+        <p>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        </p>
+      </Container>
+    );
+  }
 }
- 
+
 export default Home;
